5b63759312645212118d6cf44938ea03f488f81b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-21 21:20:18 +0700

    Create SplashScreenActivity.kt

---
+++ app/src/main/java/io/erikrios/github/githubuserapp/ui/activities/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package io.erikrios.github.githubuserapp.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.erikrios.github.githubuserapp.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
