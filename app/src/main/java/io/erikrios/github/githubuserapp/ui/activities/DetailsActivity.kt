89ed0a43b3f13d3cc18aaaa7bd4434672792b281
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-22 19:09:15 +0700

    Create DetailsActivity.kt

---
+++ app/src/main/java/io/erikrios/github/githubuserapp/ui/activities/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package io.erikrios.github.githubuserapp.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.erikrios.github.githubuserapp.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
