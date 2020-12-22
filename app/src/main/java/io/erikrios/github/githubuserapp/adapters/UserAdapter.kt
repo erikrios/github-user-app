83cbb0a4e984374993154e57f514c79ec2e5e6d3
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-22 12:20:08 +0700

    Create UserAdapter.kt

---
+++ app/src/main/java/io/erikrios/github/githubuserapp/adapters/UserAdapter.kt
@@ -0,0 +1,39 @@
+package io.erikrios.github.githubuserapp.adapters
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import io.erikrios.github.githubuserapp.databinding.ItemUserBinding
+import io.erikrios.github.githubuserapp.models.User
+
+class UserAdapter(private val context: Context, private val users: List<User>) :
+    RecyclerView.Adapter<UserAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class ViewHolder(private val binding: ItemUserBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(user: User, position: Int, clickListener: ((User) -> Unit)?) {
+            binding.apply {
+                imgAvatar.setImageResource(user.avatar)
+                tvName.text = user.name
+                tvLocation.text = user.location
+                tvUsername.text = user.username
+                tvFollowers.text = user.followers.toString()
+            }
+
+            itemView.setOnClickListener { clickListener?.let { it(user) } }
+        }
+    }
+}
