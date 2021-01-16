54145dcef274927741a73486ebf210269efdbd34
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-16 11:42:01 +0700

    Refactor the code

--- app/src/main/java/io/erikrios/github/githubuserapp/ui/activities/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/ui/activities/MainActivity.kt
@@ -12,8 +12,6 @@ import io.erikrios.github.githubuserapp.viewmodels.MainViewModel
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel: MainViewModel
-    private var users = listOf<User>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,36 +19,5 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setTheme(R.style.Theme_GithubUserApp)
         setContentView(view)
-        if (users.isEmpty()) {
-            users = getUsers()
-        }
-        setRecyclerView(users)
-    }
-
-    private fun setRecyclerView(users: List<User>) {
-        val userAdapter = UserAdapter(this, users)
-        userAdapter.setOnItemClickListener { user ->
-            val intent = Intent(this@MainActivity, DetailsActivity::class.java)
-            intent.putExtra(DetailsActivity.EXTRA_USER_KEY, user)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
-        }
-
-        binding.rvUsers.adapter = userAdapter
-    }
-
-    private fun getUsers(): List<User> {
-        viewModel = MainViewModel(
-            resources.getStringArray(R.array.username),
-            resources.getStringArray(R.array.name),
-            resources.getStringArray(R.array.location),
-            resources.getStringArray(R.array.repository),
-            resources.getStringArray(R.array.company),
-            resources.getStringArray(R.array.followers),
-            resources.getStringArray(R.array.following),
-            resources.obtainTypedArray(R.array.avatar)
-        )
-
-        return viewModel.getUsers()
     }
 }
