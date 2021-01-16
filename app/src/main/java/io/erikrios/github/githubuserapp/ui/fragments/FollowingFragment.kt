8284642b472843c54f0e7261a9dd2d6eb520765e
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-16 19:42:25 +0700

    Integrated the view binding

--- app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/FollowingFragment.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/FollowingFragment.kt
@@ -1,6 +1,28 @@
 package io.erikrios.github.githubuserapp.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import io.erikrios.github.githubuserapp.databinding.FragmentFollowingBinding
 
 class FollowingFragment : Fragment() {
+
+    private var _binding: FragmentFollowingBinding? = null
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFollowingBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
