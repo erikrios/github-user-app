94e61dc50773459c0084ce5fc0b9d33bd0259720
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-16 19:37:17 +0700

    Create FollowersFragment.kt

---
+++ app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/FollowersFragment.kt
@@ -0,0 +1,18 @@
+package io.erikrios.github.githubuserapp.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FollowersFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
