80e46cec2876e5868778dc1618929f5a6749f3ae
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-16 20:52:44 +0700

    Implements the recycler view

--- app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/FollowersFragment.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/FollowersFragment.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import io.erikrios.github.githubuserapp.R
+import io.erikrios.github.githubuserapp.adapters.UserAdapter
 import io.erikrios.github.githubuserapp.databinding.FragmentFollowersBinding
+import io.erikrios.github.githubuserapp.models.User
 import io.erikrios.github.githubuserapp.ui.fragments.DetailsFragment.Companion.USERNAME_ARG_KEY
+import io.erikrios.github.githubuserapp.viewmodels.MainViewModel
 
 class FollowersFragment : Fragment() {
 
@@ -25,12 +30,35 @@ class FollowersFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         arguments?.takeIf { it.containsKey(USERNAME_ARG_KEY) }?.apply {
-            binding?.tvUsername?.text = getString(USERNAME_ARG_KEY)
+            setRecyclerView(getUsers())
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setRecyclerView(users: List<User>) {
+        val userAdapter = UserAdapter(requireContext(), users)
+        userAdapter.setOnItemClickListener { user ->
+            Toast.makeText(requireContext(), user.name, Toast.LENGTH_SHORT).show()
+        }
+        binding?.rvFollowers?.adapter = userAdapter
+    }
+
+    private fun getUsers(): List<User> {
+        val viewModel = MainViewModel(
+            resources.getStringArray(R.array.username),
+            resources.getStringArray(R.array.name),
+            resources.getStringArray(R.array.location),
+            resources.getStringArray(R.array.repository),
+            resources.getStringArray(R.array.company),
+            resources.getStringArray(R.array.followers),
+            resources.getStringArray(R.array.following),
+            resources.obtainTypedArray(R.array.avatar)
+        )
+
+        return viewModel.getUsers()
     }
 }
