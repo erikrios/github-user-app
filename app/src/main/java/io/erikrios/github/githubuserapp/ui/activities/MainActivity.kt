299e3648e9505f46eac9b2d36b7c5443cefdd98f
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-16 11:43:49 +0700

    Refactor the code

--- app/src/main/java/io/erikrios/github/githubuserapp/ui/activities/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/ui/activities/MainActivity.kt
@@ -1,13 +1,9 @@
 package io.erikrios.github.githubuserapp.ui.activities
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.erikrios.github.githubuserapp.R
-import io.erikrios.github.githubuserapp.adapters.UserAdapter
 import io.erikrios.github.githubuserapp.databinding.ActivityMainBinding
-import io.erikrios.github.githubuserapp.models.User
-import io.erikrios.github.githubuserapp.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
