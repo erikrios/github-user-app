1b420119efe31e64faa19ed709888330fc6c36d9
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-16 20:12:40 +0700

    Refactor the code

--- app/src/main/java/io/erikrios/github/githubuserapp/adapters/DetailsPagerAdapter.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/adapters/DetailsPagerAdapter.kt
@@ -4,16 +4,16 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import io.erikrios.github.githubuserapp.ui.fragments.DetailsFragment.Companion.USERNAME_ARG_KEY
 
 class DetailsPagerAdapter(
     fragmentActivity: FragmentActivity,
     val fragments: List<Fragment>,
+    private val usernameArgKey: String,
     private val username: String
 ) :
     FragmentStateAdapter(fragmentActivity) {
     override fun createFragment(position: Int): Fragment = fragments[position].apply {
-        arguments = Bundle().apply { putString(USERNAME_ARG_KEY, username) }
+        arguments = Bundle().apply { putString(usernameArgKey, username) }
     }
 
     override fun getItemCount(): Int = fragments.size
