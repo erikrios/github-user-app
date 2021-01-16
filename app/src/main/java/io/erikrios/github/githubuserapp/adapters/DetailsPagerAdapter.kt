e03714eece04ca10fb9230b05bc601af1e7bf9c2
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-16 19:57:53 +0700

    Create DetailsPagerAdapter.kt

---
+++ app/src/main/java/io/erikrios/github/githubuserapp/adapters/DetailsPagerAdapter.kt
@@ -0,0 +1,20 @@
+package io.erikrios.github.githubuserapp.adapters
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import io.erikrios.github.githubuserapp.ui.fragments.DetailsFragment.Companion.USERNAME_ARG_KEY
+
+class DetailsPagerAdapter(
+    fragmentActivity: FragmentActivity,
+    val fragments: MutableList<Fragment>,
+    private val username: String
+) :
+    FragmentStateAdapter(fragmentActivity) {
+    override fun createFragment(position: Int): Fragment = fragments[position].apply {
+        arguments = Bundle().apply { putString(USERNAME_ARG_KEY, username) }
+    }
+
+    override fun getItemCount(): Int = fragments.size
+}
