08901dde2d518a2160e6e95773d676a8b6a9fc81
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-16 20:05:16 +0700

    Get the bundle arguments value

--- app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/FollowersFragment.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/FollowersFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import io.erikrios.github.githubuserapp.databinding.FragmentFollowersBinding
+import io.erikrios.github.githubuserapp.ui.fragments.DetailsFragment.Companion.USERNAME_ARG_KEY
 
 class FollowersFragment : Fragment() {
 
@@ -19,6 +20,13 @@ class FollowersFragment : Fragment() {
     ): View? {
         _binding = FragmentFollowersBinding.inflate(inflater, container, false)
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.takeIf { it.containsKey(USERNAME_ARG_KEY) }?.apply {
+            binding?.tvUsername?.text = getString(USERNAME_ARG_KEY)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/FollowingFragment.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/FollowingFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import io.erikrios.github.githubuserapp.databinding.FragmentFollowingBinding
+import io.erikrios.github.githubuserapp.ui.fragments.DetailsFragment.Companion.USERNAME_ARG_KEY
 
 class FollowingFragment : Fragment() {
 
@@ -19,6 +20,13 @@ class FollowingFragment : Fragment() {
     ): View? {
         _binding = FragmentFollowingBinding.inflate(inflater, container, false)
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.takeIf { it.containsKey(USERNAME_ARG_KEY) }?.apply {
+            binding?.tvUsername?.text = getString(USERNAME_ARG_KEY)
+        }
     }
 
     override fun onDestroyView() {
