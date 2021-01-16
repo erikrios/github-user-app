7c70105942746bcd22baccb33308e0ba031c001a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-16 19:39:48 +0700

    Integrated the view binding

--- app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/FollowersFragment.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/FollowersFragment.kt
@@ -5,14 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import io.erikrios.github.githubuserapp.databinding.FragmentFollowersBinding
 
 class FollowersFragment : Fragment() {
+
+    private var _binding: FragmentFollowersBinding? = null
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        _binding = FragmentFollowersBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
