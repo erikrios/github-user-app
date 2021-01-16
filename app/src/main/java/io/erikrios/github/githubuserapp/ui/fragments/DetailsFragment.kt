b035af8c2c6fc1b7b6f54bda90e58d1983d6507a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-16 14:05:26 +0700

    Set toolbar title

--- app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/DetailsFragment.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/DetailsFragment.kt
@@ -47,6 +47,8 @@ class DetailsFragment : Fragment() {
             fabOpenInBrowser.setOnClickListener {
                 openInBrowser(user)
             }
+
+            toolbar.title = user.name
         }
     }
 
