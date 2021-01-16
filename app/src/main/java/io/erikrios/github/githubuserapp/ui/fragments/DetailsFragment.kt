8ae4fd59fcf1cd9d105ccb8659ef04ddacbd7857
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-16 12:13:21 +0700

    Integrated the view binding

---
+++ app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/DetailsFragment.kt
@@ -0,0 +1,28 @@
+package io.erikrios.github.githubuserapp.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import io.erikrios.github.githubuserapp.databinding.FragmentsDetailsBinding
+
+class DetailsFragment : Fragment() {
+
+    private var _binding: FragmentsDetailsBinding? = null
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentsDetailsBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
