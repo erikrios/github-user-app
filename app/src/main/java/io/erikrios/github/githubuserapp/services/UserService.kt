5b83428b041e9bebc4d969962e95251eebcbfcc6
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-22 20:27:29 +0700

    Renamed from GithubService.kt to UserService.kt

--- app/src/main/java/io/erikrios/github/githubuserapp/services/UserService.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/services/UserService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
-interface GithubService {
+interface UserService {
 
     /**
      * GET request to search a user by keyword
