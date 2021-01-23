afb19bc2a00ed287341326bf56671c601195beff
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-23 09:02:12 +0700

    Add a method to get followers

--- app/src/main/java/io/erikrios/github/githubuserapp/ui/viewmodels/DetailsViewModel.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/ui/viewmodels/DetailsViewModel.kt
@@ -6,43 +6,89 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import io.erikrios.github.githubuserapp.repositories.UserRepository
 import io.erikrios.github.githubuserapp.ui.viewstates.UserViewState
+import io.erikrios.github.githubuserapp.ui.viewstates.UsersViewState
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class DetailsViewModel(private val repository: UserRepository) : ViewModel() {
-    private val _viewState = MutableLiveData<UserViewState>().apply {
+    private val _userViewState = MutableLiveData<UserViewState>().apply {
         value = UserViewState(loading = true)
     }
 
     val viewState: LiveData<UserViewState>
-        get() = _viewState
+        get() = _userViewState
+
+    private val _followersViewState = MutableLiveData<UsersViewState>().apply {
+        value = UsersViewState(loading = true)
+    }
+
+    val followersViewState: LiveData<UsersViewState>
+        get() = _followersViewState
+
+    private val _followingViewState = MutableLiveData<UsersViewState>().apply {
+        value = UsersViewState(loading = true)
+    }
+
+    val followingViewState: LiveData<UsersViewState>
+        get() = _followingViewState
 
     fun getUserDetails(username: String): Job {
         return viewModelScope.launch {
-            _viewState.value = UserViewState(loading = true)
+            _userViewState.value = UserViewState(loading = true)
 
             try {
                 val response = repository.getUserDetails(username)
 
                 when {
                     response.isSuccessful -> {
-                        _viewState.value = UserViewState(
+                        _userViewState.value = UserViewState(
                             loading = false,
                             responseCode = response.code(),
                             user = response.body()
                         )
                     }
                     else -> {
-                        _viewState.value = UserViewState(
+                        _userViewState.value = UserViewState(
                             loading = false,
                             responseCode = response.code(),
-                            exception = Exception("Error to get data with response code ${response.code()}")
+                            exception = Exception("Error to get data with response code ${response.code()}.")
                         )
                     }
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
-                _viewState.value = UserViewState(loading = false, exception = Exception(e))
+                _userViewState.value = UserViewState(loading = false, exception = Exception(e))
+            }
+        }
+    }
+
+    fun getFollowers(username: String): Job {
+        return viewModelScope.launch {
+            _followersViewState.value = UsersViewState(loading = true)
+
+            try {
+                val response = repository.getFollowers(username)
+
+                when {
+                    response.isSuccessful -> {
+                        _followersViewState.value = UsersViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            users = response.body()
+                        )
+                    }
+                    else -> {
+                        _followersViewState.value = UsersViewState(
+                            loading = false,
+                            responseCode = response.code(),
+                            exception = Exception("Error to get data with response code ${response.code()}.")
+                        )
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                _followersViewState.value =
+                    UsersViewState(loading = false, exception = Exception(e))
             }
         }
     }
