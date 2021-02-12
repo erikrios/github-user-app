c5ade0cacd2ec559bb22d50202a8a4bec1c813f7
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-12 22:17:05 +0700

    Add a method to handle toolbar

--- app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/SettingsFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import io.erikrios.github.githubuserapp.R
 import io.erikrios.github.githubuserapp.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
@@ -16,13 +19,27 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        handleToolbar()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun handleToolbar() {
+        binding?.toolbar?.apply {
+            navigationIcon =
+                ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_arrow_back_24)
+            setNavigationOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
     }
 }
