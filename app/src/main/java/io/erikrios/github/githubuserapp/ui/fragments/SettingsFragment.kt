9019f28f08b67953b1a1162cd8d263a2e2440856
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-12 20:27:14 +0700

    Integrated the view binding

--- app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/SettingsFragment.kt
@@ -5,15 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import io.erikrios.github.githubuserapp.R
+import io.erikrios.github.githubuserapp.databinding.FragmentSearchBinding
 
 class SettingsFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
