5516a6fe3d0ae3d699d095675e526d4345f2f4e7
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-13 14:23:15 +0700

    Create FavoritesViewModel.kt

---
+++ app/src/main/java/io/erikrios/github/githubuserapp/ui/viewmodels/FavoritesViewModel.kt
@@ -0,0 +1,27 @@
+package io.erikrios.github.githubuserapp.ui.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import io.erikrios.github.githubuserapp.repositories.UserRepository
+import io.erikrios.github.githubuserapp.ui.viewstates.UsersViewState
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class FavoritesViewModel(private val repository: UserRepository) : ViewModel() {
+    private val _usersViewState = MutableLiveData<UsersViewState>().apply {
+        value = UsersViewState(loading = true)
+    }
+
+    val usersViewState: LiveData<UsersViewState> get() = _usersViewState
+
+    private fun getFavoriteUsers(): Job {
+        return viewModelScope.launch {
+            _usersViewState.value = UsersViewState(loading = true)
+
+            val users = repository.getUsers()
+            _usersViewState.value = UsersViewState(loading = false, users = users)
+        }
+    }
+}
