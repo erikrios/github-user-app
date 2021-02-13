ee28235838913902cb21e13ca6b21a16da8fb7a0
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-13 10:53:25 +0700

    Add a method to handle toolbar

--- app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/FavoritesFragment.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/FavoritesFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import io.erikrios.github.githubuserapp.R
 import io.erikrios.github.githubuserapp.databinding.FragmentFavoritesBinding
 
 class FavoritesFragment : Fragment() {
@@ -18,5 +21,20 @@ class FavoritesFragment : Fragment() {
     ): View? {
         _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        handleToolbar()
+    }
+
+    private fun handleToolbar() {
+        binding?.toolbar?.apply {
+            navigationIcon =
+                ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_arrow_back_24)
+            setNavigationOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
     }
 }
