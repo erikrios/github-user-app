edf47dfcaeabf170e11a375f94648bc551346402
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-13 12:01:40 +0700

    Create UserDao.kt

---
+++ app/src/main/java/io/erikrios/github/githubuserapp/databases/UserDao.kt
@@ -0,0 +1,23 @@
+package io.erikrios.github.githubuserapp.databases
+
+import androidx.room.*
+import io.erikrios.github.githubuserapp.models.User
+import io.erikrios.github.githubuserapp.models.User.Companion.COLUMN_ID
+import io.erikrios.github.githubuserapp.models.User.Companion.COLUMN_USERNAME
+import io.erikrios.github.githubuserapp.models.User.Companion.TABLE_NAME
+
+@Dao
+interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(user: User): Long
+
+    @Query("SELECT * FROM $TABLE_NAME ORDER BY $COLUMN_USERNAME")
+    suspend fun getUsers(): List<User>
+
+    @Query("SELECT * FROM $TABLE_NAME WHERE $COLUMN_ID = :id")
+    suspend fun getUser(id: Long): User
+
+    @Delete
+    suspend fun deleteUser(user: User)
+}
