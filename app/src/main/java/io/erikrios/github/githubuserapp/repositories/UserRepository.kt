1316a5db0f9d6d9f38a6e90f6797856edb9f4252
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-13 12:24:18 +0700

    Add some methods

--- app/src/main/java/io/erikrios/github/githubuserapp/repositories/UserRepository.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/repositories/UserRepository.kt
@@ -1,9 +1,11 @@
 package io.erikrios.github.githubuserapp.repositories
 
+import io.erikrios.github.githubuserapp.databases.UserDatabase
+import io.erikrios.github.githubuserapp.models.User
 import io.erikrios.github.githubuserapp.services.ServiceBuilder
 import io.erikrios.github.githubuserapp.services.UserService
 
-class UserRepository {
+class UserRepository(private val db: UserDatabase? = null) {
 
     private val userService = ServiceBuilder.buildService(UserService::class.java)
 
@@ -14,4 +16,12 @@ class UserRepository {
     suspend fun getFollowers(username: String) = userService.getFollowers(username)
 
     suspend fun getFollowing(username: String) = userService.getFollowing(username)
+
+    suspend fun insert(user: User) = db?.getUserDao()?.insert(user)
+
+    fun getUsers() = db?.getUserDao()?.getUsers()
+
+    fun getUser(id: Long) = db?.getUserDao()?.getUser(id)
+
+    suspend fun deleteUser(user: User) = db?.getUserDao()?.deleteUser(user)
 }
