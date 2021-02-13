45271dd57e0b6e68ca90d97e6b06bdaa1c093d53
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-13 14:36:41 +0700

    Integrated the view model

--- app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/FavoritesFragment.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/FavoritesFragment.kt
@@ -6,29 +6,40 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import io.erikrios.github.githubuserapp.R
 import io.erikrios.github.githubuserapp.adapters.UserAdapter
+import io.erikrios.github.githubuserapp.databases.UserDatabase
 import io.erikrios.github.githubuserapp.databinding.FragmentFavoritesBinding
 import io.erikrios.github.githubuserapp.models.User
+import io.erikrios.github.githubuserapp.repositories.UserRepository
+import io.erikrios.github.githubuserapp.ui.viewmodels.FavoritesViewModel
+import io.erikrios.github.githubuserapp.ui.viewmodels.FavoritesViewModelFactory
+import io.erikrios.github.githubuserapp.ui.viewstates.UsersViewState
 
 class FavoritesFragment : Fragment() {
 
     private var _binding: FragmentFavoritesBinding? = null
     private val binding get() = _binding
+    private lateinit var viewModel: FavoritesViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        val repository = UserRepository(UserDatabase(requireContext()))
+        val factory = FavoritesViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, factory).get(FavoritesViewModel::class.java)
         return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         handleToolbar()
+        viewModel.usersViewState.observe(viewLifecycleOwner, this@FavoritesFragment::handleState)
     }
 
     override fun onDestroyView() {
@@ -42,6 +53,38 @@ class FavoritesFragment : Fragment() {
                 ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_arrow_back_24)
             setNavigationOnClickListener {
                 findNavController().popBackStack()
+            }
+        }
+    }
+
+    private fun handleState(usersViewState: UsersViewState) {
+        handleLoading(usersViewState.loading)
+        usersViewState.users?.let { handleSuccess(it) }
+    }
+
+    private fun handleLoading(isLoading: Boolean) {
+        binding?.apply {
+            if (isLoading) {
+                rvUsers.visibility = View.GONE
+                progressBar.visibility = View.VISIBLE
+            } else {
+                progressBar.visibility = View.GONE
+                rvUsers.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun handleSuccess(users: List<User>) {
+        if (users.isEmpty()) {
+            binding?.apply {
+                rvUsers.visibility = View.GONE
+                lavEmpty.visibility = View.VISIBLE
+            }
+        } else {
+            binding?.apply {
+                lavEmpty.visibility = View.GONE
+                rvUsers.visibility = View.VISIBLE
+                setRecyclerView(users)
             }
         }
     }
