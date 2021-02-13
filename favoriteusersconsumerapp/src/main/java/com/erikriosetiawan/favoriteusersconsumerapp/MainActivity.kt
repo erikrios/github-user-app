09beb3dd5848228594310b6539e18a7d30041d01
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-13 19:00:42 +0700

    Add a method to set recycler view

--- favoriteusersconsumerapp/src/main/java/com/erikriosetiawan/favoriteusersconsumerapp/MainActivity.kt
+++ favoriteusersconsumerapp/src/main/java/com/erikriosetiawan/favoriteusersconsumerapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.erikriosetiawan.favoriteusersconsumerapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.erikriosetiawan.favoriteusersconsumerapp.adapters.UserAdapter
 import com.erikriosetiawan.favoriteusersconsumerapp.databinding.ActivityMainBinding
+import com.erikriosetiawan.favoriteusersconsumerapp.models.User
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +15,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    private fun setRecyclerView(users: List<User>) {
+        val adapter = UserAdapter(this, users)
+        adapter.setOnItemClickListener { user ->
+            Toast.makeText(this, user.name, Toast.LENGTH_SHORT).show()
+        }
+        binding.rvUsers.adapter = adapter
     }
 }
