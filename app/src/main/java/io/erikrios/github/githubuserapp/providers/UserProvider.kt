c30786e43dba6052539775867d6fda27b57b2e69
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-13 17:40:23 +0700

    Implements the content provider

--- app/src/main/java/io/erikrios/github/githubuserapp/providers/UserProvider.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/providers/UserProvider.kt
@@ -1,42 +1,113 @@
 package io.erikrios.github.githubuserapp.providers
 
 import android.content.ContentProvider
+import android.content.ContentUris
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
+import io.erikrios.github.githubuserapp.databases.DatabaseContract.AUTHORITY
+import io.erikrios.github.githubuserapp.databases.DatabaseContract.UserColumns.Companion.TABLE_NAME
+import io.erikrios.github.githubuserapp.databases.DatabaseContract.UserColumns.Companion.fromContentValues
+import io.erikrios.github.githubuserapp.databases.UserDatabase
+import kotlinx.coroutines.runBlocking
 
 class UserProvider : ContentProvider() {
 
-    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
-        TODO("Implement this to handle requests to delete one or more rows")
-    }
+    companion object {
+        private const val USER = 1
+        private const val USER_ID = 2
+        private val sUriMather = UriMatcher(UriMatcher.NO_MATCH)
 
-    override fun getType(uri: Uri): String? {
-        TODO(
-            "Implement this to handle requests for the MIME type of the data" +
-                    "at the given URI"
-        )
-    }
+        init {
+            // content://io.erikrios.github.githubuserapp/users
+            sUriMather.addURI(AUTHORITY, TABLE_NAME, USER)
 
-    override fun insert(uri: Uri, values: ContentValues?): Uri? {
-        TODO("Implement this to handle requests to insert a new row.")
+            // content://io.erikrios.github.githubuserapp/users/id
+            sUriMather.addURI(AUTHORITY, "$TABLE_NAME/#", USER_ID)
+        }
     }
 
     override fun onCreate(): Boolean {
-        TODO("Implement this to initialize your content provider on startup.")
+        return true
     }
 
     override fun query(
         uri: Uri, projection: Array<String>?, selection: String?,
         selectionArgs: Array<String>?, sortOrder: String?
-    ): Cursor? {
-        TODO("Implement this to handle query requests from clients.")
+    ): Cursor {
+        return when (sUriMather.match(uri)) {
+            USER ->
+                runBlocking {
+                    UserDatabase(requireContext()).getUserDao().getFavoriteUsers()
+                }
+            USER_ID ->
+                runBlocking {
+                    UserDatabase(requireContext()).getUserDao()
+                        .getFavoriteUser(uri.lastPathSegment.toString().toLong())
+                }
+            else -> throw IllegalArgumentException("Unknown URI: $uri")
+        }
+    }
+
+    override fun getType(uri: Uri): String {
+        return when (sUriMather.match(uri)) {
+            USER ->
+                "vnd.android.cursor.dir/$AUTHORITY.$TABLE_NAME"
+            USER_ID ->
+                "vnd.android.cursor.item/$AUTHORITY.$TABLE_NAME"
+            else ->
+                throw IllegalArgumentException("Unknown URI: $uri")
+        }
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        return when (sUriMather.match(uri)) {
+            USER -> {
+                runBlocking {
+                    val id = UserDatabase(requireContext()).getUserDao()
+                        .insert(fromContentValues(values))
+                    context?.contentResolver?.notifyChange(uri, null)
+                    ContentUris.withAppendedId(uri, id)
+                }
+            }
+            USER_ID -> throw IllegalArgumentException("Invalid URI, cannot insert with ID: $uri")
+            else -> throw IllegalArgumentException("Unknown URI: $uri")
+        }
     }
 
     override fun update(
         uri: Uri, values: ContentValues?, selection: String?,
         selectionArgs: Array<String>?
     ): Int {
-        TODO("Implement this to handle requests to update one or more rows.")
+        return when (sUriMather.match(uri)) {
+            USER -> throw IllegalArgumentException("Invalid URI, cannot update without ID: $uri")
+            USER_ID -> {
+                val id = uri.lastPathSegment.toString().toLong()
+                val user = fromContentValues(values)
+                user.id = id
+                runBlocking {
+                    val count = UserDatabase(requireContext()).getUserDao().updateUser(user)
+                    context?.contentResolver?.notifyChange(uri, null)
+                    count
+                }
+            }
+            else -> throw IllegalArgumentException("Unknown URI: $uri")
+        }
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
+        return when (sUriMather.match(uri)) {
+            USER -> throw IllegalArgumentException("Invalid URI, cannot delete without ID: $uri")
+            USER_ID -> {
+                val id = uri.lastPathSegment.toString().toLong()
+                runBlocking {
+                    val count = UserDatabase(requireContext()).getUserDao().deleteUserById(id)
+                    context?.contentResolver?.notifyChange(uri, null)
+                    count
+                }
+            }
+            else -> throw IllegalArgumentException("Unknown URI: $uri")
+        }
     }
 }
