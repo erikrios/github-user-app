270794c06bcbf69623a8602b224b1a0cceb15709
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-14 15:09:52 +0700

    Implements the alarm manager with notification

--- app/src/main/java/io/erikrios/github/githubuserapp/receivers/AlarmReceiver.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/receivers/AlarmReceiver.kt
@@ -1,15 +1,21 @@
 package io.erikrios.github.githubuserapp.receivers
 
+import android.app.AlarmManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
 import android.os.Build
+import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import io.erikrios.github.githubuserapp.R
+import io.erikrios.github.githubuserapp.ui.activities.MainActivity
+import java.util.*
 
 class AlarmReceiver : BroadcastReceiver() {
 
@@ -30,6 +36,44 @@ class AlarmReceiver : BroadcastReceiver() {
         showAlarmNotification(context, title.toString(), message.toString(), notificationId)
     }
 
+    fun setReminderAlarm(context: Context, type: String, title: String, message: String) {
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, AlarmReceiver::class.java)
+        intent.apply {
+            putExtra(EXTRA_TYPE, type)
+            putExtra(EXTRA_TITLE, title)
+            putExtra(EXTRA_MESSAGE, message)
+        }
+
+        val calendar = Calendar.getInstance()
+        calendar.apply {
+            set(Calendar.HOUR_OF_DAY, 9)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+
+        val pendingIntent = PendingIntent.getBroadcast(context, ID_REMINDER, intent, 0)
+        alarmManager.setInexactRepeating(
+            AlarmManager.RTC_WAKEUP,
+            calendar.timeInMillis,
+            AlarmManager.INTERVAL_DAY,
+            pendingIntent
+        )
+
+        Toast.makeText(context, R.string.reminder_set_up, Toast.LENGTH_SHORT).show()
+    }
+
+    fun cancelAlarm(context: Context, type: String) {
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, AlarmReceiver::class.java)
+        val requestCode = if (type.equals(TYPE_REMINDER, ignoreCase = true)) ID_REMINDER else -1
+        val pendingIntent = PendingIntent.getBroadcast(context, requestCode, intent, 0)
+        pendingIntent.cancel()
+        alarmManager.cancel(pendingIntent)
+        Toast.makeText(context, R.string.reminder_canceled, Toast.LENGTH_SHORT).show()
+    }
+
     private fun showAlarmNotification(
         context: Context,
         title: String,
@@ -39,10 +83,18 @@ class AlarmReceiver : BroadcastReceiver() {
         val channelId = "Erik_Notification_Channel"
         val channelName = "Erik_Alarm_Manager_Channel"
 
+        val notifyIntent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val notifyPendingIntent =
+            PendingIntent.getActivity(context, 0, notifyIntent, FLAG_UPDATE_CURRENT)
+
         val notificationManagerCompat =
             context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val builder = NotificationCompat.Builder(context, channelId)
+            .setContentIntent(notifyPendingIntent)
             .setSmallIcon(R.drawable.ic_baseline_notifications_active_24)
             .setContentTitle(title)
             .setContentText(message)
