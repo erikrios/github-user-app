5d3be99bb3f0af929cdffb4a42ae43222250fde3
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-14 15:15:45 +0700

    Add a method to set reminder alarm and cancel alarm

--- app/src/main/java/io/erikrios/github/githubuserapp/ui/viewmodels/SettingsViewModel.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/ui/viewmodels/SettingsViewModel.kt
@@ -1,14 +1,18 @@
 package io.erikrios.github.githubuserapp.ui.viewmodels
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import io.erikrios.github.githubuserapp.datastrores.SettingPreferences
+import io.erikrios.github.githubuserapp.receivers.AlarmReceiver
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class SettingsViewModel(private val preferences: SettingPreferences) : ViewModel() {
+
+    private var alarmReceiver = AlarmReceiver()
 
     fun setNotificationState(isActive: Boolean): Job {
         return viewModelScope.launch {
@@ -18,5 +22,13 @@ class SettingsViewModel(private val preferences: SettingPreferences) : ViewModel
 
     fun isNotificationActive(): LiveData<Boolean> {
         return preferences.isNotificationActive.asLiveData()
+    }
+
+    fun setReminderAlarm(context: Context, type: String, title: String, message: String) {
+        alarmReceiver.setReminderAlarm(context, type, title, message)
+    }
+
+    fun cancelAlarm(context: Context, type: String) {
+        alarmReceiver.cancelAlarm(context, type)
     }
 }
