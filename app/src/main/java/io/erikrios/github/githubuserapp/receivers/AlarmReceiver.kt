2a0581e6baf06340e9fba73b942da7c4ae30a716
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-14 14:38:36 +0700

    Add a method to show alarm notification

--- app/src/main/java/io/erikrios/github/githubuserapp/receivers/AlarmReceiver.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/receivers/AlarmReceiver.kt
@@ -1,13 +1,69 @@
 package io.erikrios.github.githubuserapp.receivers
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import io.erikrios.github.githubuserapp.R
 
 class AlarmReceiver : BroadcastReceiver() {
 
+    companion object {
+        const val TYPE_REMINDER = "ReminderAlarm"
+        const val EXTRA_TITLE = "extra_title"
+        const val EXTRA_MESSAGE = "extra_message"
+        const val EXTRA_TYPE = "extra_type"
+        private const val ID_REMINDER = 100
+    }
+
     override fun onReceive(context: Context, intent: Intent) {
-        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-        TODO("AlarmReceiver.onReceive() is not implemented")
+        val title = intent.getStringExtra(EXTRA_TITLE)
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        val type = intent.getStringExtra(EXTRA_TYPE)
+        val notificationId = if (type.equals(TYPE_REMINDER, ignoreCase = true)) ID_REMINDER else -1
+
+        showAlarmNotification(context, title.toString(), message.toString(), notificationId)
+    }
+
+    private fun showAlarmNotification(
+        context: Context,
+        title: String,
+        message: String,
+        notificationId: Int
+    ) {
+        val channelId = "Erik_Notification_Channel"
+        val channelName = "Erik_Alarm_Manager_Channel"
+
+        val notificationManagerCompat =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val builder = NotificationCompat.Builder(context, channelId)
+            .setSmallIcon(R.drawable.ic_baseline_notifications_active_24)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setColor(ContextCompat.getColor(context, android.R.color.transparent))
+            .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
+            .setSound(alarmSound)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            channel.enableVibration(true)
+            channel.vibrationPattern = longArrayOf(1000, 1000, 1000, 1000, 1000)
+
+            builder.setChannelId(channelId)
+            notificationManagerCompat.createNotificationChannel(channel)
+        }
+        val notification = builder.build()
+        notificationManagerCompat.notify(notificationId, notification)
     }
 }
