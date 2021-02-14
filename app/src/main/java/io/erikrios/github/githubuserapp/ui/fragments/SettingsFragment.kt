dac0296b525725f1b2dcc88d3da01bc182600955
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-14 13:56:18 +0700

    Implements the datastore preferences

--- app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/ui/fragments/SettingsFragment.kt
@@ -6,26 +6,40 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import io.erikrios.github.githubuserapp.R
 import io.erikrios.github.githubuserapp.databinding.FragmentSettingsBinding
+import io.erikrios.github.githubuserapp.datastrores.SettingPreferences
+import io.erikrios.github.githubuserapp.ui.viewmodels.SettingsViewModel
+import io.erikrios.github.githubuserapp.ui.viewmodels.SettingsViewModelFactory
 
 class SettingsFragment : Fragment() {
 
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding
+    private lateinit var viewModel: SettingsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        val factory = SettingsViewModelFactory(SettingPreferences(requireContext()))
+        viewModel = ViewModelProvider(this, factory).get(SettingsViewModel::class.java)
         return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         handleToolbar()
+        viewModel.isNotificationActive().observe(viewLifecycleOwner) { isActive ->
+            binding?.switchReminder?.isChecked = isActive
+        }
+
+        binding?.switchReminder?.setOnCheckedChangeListener { _, isChecked ->
+            viewModel.setNotificationState(isChecked)
+        }
     }
 
     override fun onDestroyView() {
