edfd052ee6ffb60fa9be0dea4ccb1af2e2707876
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-09 19:18:49 +0700

    Implements the widget

--- app/src/main/java/io/erikrios/github/githubuserapp/widgets/GithubUserWidget.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/widgets/GithubUserWidget.kt
@@ -1,45 +1,68 @@
 package io.erikrios.github.githubuserapp.widgets
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
+import android.widget.Toast
+import androidx.core.net.toUri
 import io.erikrios.github.githubuserapp.R
 
-/**
- * Implementation of App Widget functionality.
- */
 class GithubUserWidget : AppWidgetProvider() {
+
+    companion object {
+        private const val TOAST_ACTION = "io.erikrios.github.githubuserapp.TOAST_ACTION"
+        const val EXTRA_NAME = "io.erikrios.github.githubuserapp.EXTRA_ITEM"
+
+        private fun updateAppWidget(
+            context: Context,
+            appWidgetManager: AppWidgetManager,
+            appWidgetId: Int
+        ) {
+            val intent = Intent(context, StackWidgetService::class.java)
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+            intent.data = intent.toUri(Intent.URI_INTENT_SCHEME).toUri()
+
+            val views = RemoteViews(context.packageName, R.layout.github_user_widget)
+            views.setRemoteAdapter(R.id.stack_view, intent)
+            views.setEmptyView(R.id.stack_view, R.id.empt_view)
+
+            val toastIntent = Intent(context, GithubUserWidget::class.java)
+            toastIntent.action = TOAST_ACTION
+            toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+            intent.data = intent.toUri(Intent.URI_INTENT_SCHEME).toUri()
+            val toastPendingIntent =
+                PendingIntent.getBroadcast(
+                    context,
+                    0,
+                    toastIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+            views.setPendingIntentTemplate(R.id.stack_view, toastPendingIntent)
+
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+        intent?.action?.let {
+            if (it == TOAST_ACTION) {
+                val name = intent.getStringExtra(EXTRA_NAME)
+                Toast.makeText(context, name, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
     }
-
-    override fun onEnabled(context: Context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    override fun onDisabled(context: Context) {
-        // Enter relevant functionality for when the last widget is disabled
-    }
-}
-
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-    val widgetText = context.getString(R.string.appwidget_text)
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.github_user_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
-
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
 }
