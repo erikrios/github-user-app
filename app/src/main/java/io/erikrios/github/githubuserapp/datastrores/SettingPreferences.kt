67df385a722ad3bc0a413b55fd8096b99c109b0a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-09 12:10:18 +0700

    Refactor the code

--- app/src/main/java/io/erikrios/github/githubuserapp/datastrores/SettingPreferences.kt
+++ app/src/main/java/io/erikrios/github/githubuserapp/datastrores/SettingPreferences.kt
@@ -1,30 +1,25 @@
 package io.erikrios.github.githubuserapp.datastrores
 
 import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.createDataStore
+import io.erikrios.github.githubuserapp.utils.settingPreferences
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
-class SettingPreferences(context: Context) {
-
-    private val dataStore: DataStore<Preferences> =
-        context.createDataStore(name = "setting_preferences")
+class SettingPreferences(private val context: Context) {
 
     companion object {
         val NOTIFICATION_STATE_KEY = booleanPreferencesKey("notification_state")
     }
 
     suspend fun setNotificationState(isActive: Boolean) {
-        dataStore.edit { preferences ->
+        context.settingPreferences.edit { preferences ->
             preferences[NOTIFICATION_STATE_KEY] = isActive
         }
     }
 
-    val isNotificationActive: Flow<Boolean> = dataStore.data
+    val isNotificationActive: Flow<Boolean> = context.settingPreferences.data
         .map { preferences ->
             preferences[NOTIFICATION_STATE_KEY] ?: false
         }
