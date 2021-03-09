ca826561ad4b09721faf9d15a50f7c845e4ecc2c
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-09 11:37:29 +0700

    Remove unnecessary method

--- favoriteusersconsumerapp/src/main/java/com/erikriosetiawan/favoriteusersconsumerapp/databases/DatabaseContract.kt
+++ favoriteusersconsumerapp/src/main/java/com/erikriosetiawan/favoriteusersconsumerapp/databases/DatabaseContract.kt
@@ -37,52 +37,6 @@ object DatabaseContract {
                 .appendPath(TABLE_NAME)
                 .build()
 
-            fun fromContentValues(values: ContentValues?): User {
-                val user = User()
-                values?.let { contentValues ->
-                    contentValues.apply {
-                        if (containsKey(COLUMN_ID)) user.id = getAsLong(COLUMN_ID)
-                        if (containsKey(COLUMN_USERNAME)) user.username =
-                            getAsString(COLUMN_USERNAME)
-                        if (containsKey(COLUMN_NAME)) user.name = getAsString(COLUMN_NAME)
-                        if (containsKey(COLUMN_LOCATION)) user.location =
-                            getAsString(COLUMN_LOCATION)
-                        if (containsKey(COLUMN_PUBLIC_REPOSITORIES)) user.publicRepositories =
-                            getAsInteger(
-                                COLUMN_PUBLIC_REPOSITORIES
-                            )
-                        if (containsKey(COLUMN_PUBLIC_GISTS)) user.publicGists = getAsInteger(
-                            COLUMN_PUBLIC_GISTS
-                        )
-                        if (containsKey(COLUMN_COMPANY)) user.company =
-                            getAsString(COLUMN_COMPANY)
-                        if (containsKey(COLUMN_FOLLOWERS)) user.followers = getAsInteger(
-                            COLUMN_FOLLOWERS
-                        )
-                        if (containsKey(COLUMN_FOLLOWING)) user.following = getAsInteger(
-                            COLUMN_FOLLOWING
-                        )
-                        if (containsKey(COLUMN_AVATAR_URL)) user.avatarUrl = getAsString(
-                            COLUMN_AVATAR_URL
-                        )
-                        if (containsKey(COLUMN_TYPE)) user.type = getAsString(COLUMN_TYPE)
-                        if (containsKey(COLUMN_HTML_URL)) user.htmlUrl =
-                            getAsString(COLUMN_HTML_URL)
-                        if (containsKey(COLUMN_BLOG)) user.blog = getAsString(COLUMN_BLOG)
-                        if (containsKey(COLUMN_BIO)) user.bio = getAsString(COLUMN_BIO)
-                        if (containsKey(COLUMN_HIREABLE)) user.hireable =
-                            getAsBoolean(COLUMN_HIREABLE)
-                        if (containsKey(COLUMN_CREATED_AT)) user.createdAt = getAsString(
-                            COLUMN_CREATED_AT
-                        )
-                        if (containsKey(COLUMN_UPDATED_AT)) user.updatedAt = getAsString(
-                            COLUMN_UPDATED_AT
-                        )
-                    }
-                }
-                return user
-            }
-
             fun toContentValues(user: User): ContentValues {
                 val values = ContentValues()
                 user.apply {
