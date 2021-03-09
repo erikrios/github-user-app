f2b9ce395f648d7f3988a9bf18b31ea495d2eef7
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-09 09:44:50 +0700

    Integrated the view binding

--- favoriteusersconsumerapp/src/main/java/com/erikriosetiawan/favoriteusersconsumerapp/DetailsActivity.kt
+++ favoriteusersconsumerapp/src/main/java/com/erikriosetiawan/favoriteusersconsumerapp/DetailsActivity.kt
@@ -1,11 +1,16 @@
 package com.erikriosetiawan.favoriteusersconsumerapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.erikriosetiawan.favoriteusersconsumerapp.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
