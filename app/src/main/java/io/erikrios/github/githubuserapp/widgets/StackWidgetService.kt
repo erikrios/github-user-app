9dd8123edb8eb2e2c58215a950f36fd9d55c3aae
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-09 19:48:14 +0700

    Create StackWidgetService.kt

---
+++ app/src/main/java/io/erikrios/github/githubuserapp/widgets/StackWidgetService.kt
@@ -0,0 +1,9 @@
+package io.erikrios.github.githubuserapp.widgets
+
+import android.content.Intent
+import android.widget.RemoteViewsService
+
+class StackWidgetService : RemoteViewsService() {
+    override fun onGetViewFactory(intent: Intent?): RemoteViewsFactory =
+        StackRemoteViewsFactory(this.applicationContext)
+}
